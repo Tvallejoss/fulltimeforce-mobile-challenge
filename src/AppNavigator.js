42bf304e4b5080b18bd194d649acb4e01e85e716
= <tvallejos@stgroup.com.ar>
2025-05-09 15:54:25 -0300

    Add ProfileCard component with dynamic filters and background gradient per category

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,3 +1,4 @@
+// Hooks
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import DrawerNavigator from "./navigation/DrawerNavigator";
---
+++ src/data/mockProfiles.js
@@ -0,0 +1,29 @@
+export const MOCK_PROFILES = {
+  friendship: [
+    {
+      name: "Sandra Gómez",
+      age: 21,
+      location: "Surco, Perú",
+      image:
+        "https://cdn.discordapp.com/attachments/1095387607409635330/1370449425326932190/faff896ca6910748644aa65711411f7281ff1a3e.jpg?ex=681f89fc&is=681e387c&hm=5284c94ec2c3bbf8dd0b4fe50f8214b9dc142f8290bf4abdd58843770a89626d&",
+    },
+  ],
+  love: [
+    {
+      name: "Beatriz",
+      age: 25,
+      location: "Miraflores, Perú",
+      image:
+        "https://cdn.discordapp.com/attachments/1095387607409635330/1370451126633234522/6e48d4678dc36649f97605fec8ce9b8b39059f8f.png?ex=681f8b92&is=681e3a12&hm=acc34c99b9c25ad7563a90191efbbc3ceeb995701f4a9bf388785e17cb5fa17d&",
+    },
+  ],
+  relationship: [
+    {
+      name: "Valentina Ríos",
+      age: 23,
+      location: "Barranco, Perú",
+      image:
+        "https://cdn.discordapp.com/attachments/1095387607409635330/1370450556761538611/c74e3ede567bf0381a39e776aa384fea784f421a.jpg?ex=681f8b0a&is=681e398a&hm=d17ba043fe15bdd262aeb730d40c202056702c3e55f0783a1b8a2164ed3c98f8&",
+    },
+  ],
+};
