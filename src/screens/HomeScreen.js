2872b2947821ea8b3f4c8f9f6c7a39039eb7e77b
= <tvallejos@stgroup.com.ar>
2025-05-09 13:16:48 -0300

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Testing Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fafafa",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});
